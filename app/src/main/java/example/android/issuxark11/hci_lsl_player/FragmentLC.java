d417ec11db45fecaf1ba59a7f7e62bdc690a14a2
issuxark11 <issuxark11@gmail.com>
2014-11-30 02:59:34 +0900

    function added - script next step : spinner

--- app/src/main/java/example/android/issuxark11/hci_lsl_player/FragmentLC.java
+++ app/src/main/java/example/android/issuxark11/hci_lsl_player/FragmentLC.java
@@ -5,14 +5,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * Created by 영진 on 2014-11-29.
  */
 public class FragmentLC extends Fragment{
+    private static String ScriptURL = "http://lslscript.pe.hu/script.html";
+    WebView webview;   // script 창을 불러온다.
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_lc,container,false);
 
-        return inflater.inflate(R.layout.fragment_lc, container, false);
+        webview = (WebView) view.findViewById(R.id.ScriptView);
+        webview.post(new Runnable() {
+            @Override
+            public void run() {
+                webview.setHorizontalScrollBarEnabled(false); // 세로 scroll 제거
+                webview.setVerticalScrollBarEnabled(false);   // 가로 scroll 제거
+                webview.getSettings().setUseWideViewPort(true);
+                webview.setInitialScale(1);
+                webview.loadUrl(ScriptURL);
+            }
+        });
+        return view;
     }
 }
