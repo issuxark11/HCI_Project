a2ac4fc93062668c8a035200c2069f9a4b9a47a5
issuxark11 <issuxark11@gmail.com>
2014-11-30 03:21:40 +0900

    function added - spinner next step : change bottom by clicking                     spinner customizing

--- app/src/main/java/example/android/issuxark11/hci_lsl_player/FragmentLC.java
+++ app/src/main/java/example/android/issuxark11/hci_lsl_player/FragmentLC.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by 영진 on 2014-11-29.
@@ -13,7 +16,7 @@ import android.webkit.WebView;
 public class FragmentLC extends Fragment{
     private static String ScriptURL = "http://lslscript.pe.hu/script.html";
     WebView webview;   // script 창을 불러온다.
-
+    Spinner spinner;   // 배속 선택
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_lc,container,false);
@@ -29,6 +32,21 @@ public class FragmentLC extends Fragment{
                 webview.loadUrl(ScriptURL);
             }
         });
+
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.speed_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+                }
+
+                public void onNothingSelected(AdapterView<?> arg0) {
+
+                }
+        });
+
         return view;
     }
 }
