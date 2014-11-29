3da553891e4168820b97b9934813db7dab69c0d5
issuxark11 <issuxark11@gmail.com>
2014-11-30 04:03:42 +0900

    UI changed - spinner next step : change button by onClick

--- app/src/main/java/example/android/issuxark11/hci_lsl_player/FragmentLC.java
+++ app/src/main/java/example/android/issuxark11/hci_lsl_player/FragmentLC.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 /**
@@ -16,6 +17,10 @@ import android.widget.Spinner;
 public class FragmentLC extends Fragment{
     private static String ScriptURL = "http://lslscript.pe.hu/script.html";
     WebView webview;   // script 창을 불러온다.
+    Button stopbtn;
+    Button playbtn;
+    Button pausebtn;
+    Button folderbtn;
     Spinner spinner;   // 배속 선택
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -37,14 +42,15 @@ public class FragmentLC extends Fragment{
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.speed_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setSelection(1);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
 
-                }
+            }
 
-                public void onNothingSelected(AdapterView<?> arg0) {
+            public void onNothingSelected(AdapterView<?> arg0) {
 
-                }
+            }
         });
 
         return view;
